5dafa3a3620e53baf9140655cc41bd4c7f20ab28
RPGoor <ryanpgoor@gmail.com>
2026-08-27 00:22:28 +0100

    mouse, keyboard and window handle cleanup unused

--- Source/Engine.Platform/Input/Mouse.cpp
+++ Source/Engine.Platform/Input/Mouse.cpp
@@ -18,45 +18,9 @@ std::optional<Mouse::RawDelta> Mouse::ReadRawDelta() noexcept
     return d;
 }
 
-int Mouse::GetPosX() const noexcept
-{
-    return x;
-}
-
-int Mouse::GetPosY() const noexcept
-{
-    return y;
-}
-
 bool Mouse::IsInWindow() const noexcept
 {
     return isInWindow;
-}
-
-bool Mouse::LeftIsPressed() const noexcept
-{
-    return leftIsPressed;
-}
-
-bool Mouse::RightIsPressed() const noexcept
-{
-    return rightIsPressed;
-}
-
-std::optional<Mouse::Event> Mouse::Read() noexcept
-{
-    if (buffer.size() > 0u)
-    {
-        Mouse::Event e = buffer.front();
-        buffer.pop();
-        return e;
-    }
-    return {};
-}
-
-void Mouse::Flush() noexcept
-{
-    buffer = std::queue<Event>();
 }
 
 void Mouse::EnableRaw() noexcept
@@ -100,51 +64,7 @@ void Mouse::OnMouseEnter() noexcept
 void Mouse::OnRawDelta(int dx, int dy) noexcept
 {
     rawDeltaBuffer.push({ dx,dy });
-    TrimBuffer();
-}
-
-void Mouse::OnLeftPressed(int x, int y) noexcept
-{
-    leftIsPressed = true;
-
-    buffer.push(Mouse::Event(Mouse::Event::Type::LPress, *this));
-    TrimBuffer();
-}
-
-void Mouse::OnLeftReleased(int x, int y) noexcept
-{
-    leftIsPressed = false;
-
-    buffer.push(Mouse::Event(Mouse::Event::Type::LRelease, *this));
-    TrimBuffer();
-}
-
-void Mouse::OnRightPressed(int x, int y) noexcept
-{
-    rightIsPressed = true;
-
-    buffer.push(Mouse::Event(Mouse::Event::Type::RPress, *this));
-    TrimBuffer();
-}
-
-void Mouse::OnRightReleased(int x, int y) noexcept
-{
-    rightIsPressed = false;
-
-    buffer.push(Mouse::Event(Mouse::Event::Type::RRelease, *this));
-    TrimBuffer();
-}
-
-void Mouse::OnWheelUp(int x, int y) noexcept
-{
-    buffer.push(Mouse::Event(Mouse::Event::Type::WheelUp, *this));
-    TrimBuffer();
-}
-
-void Mouse::OnWheelDown(int x, int y) noexcept
-{
-    buffer.push(Mouse::Event(Mouse::Event::Type::WheelDown, *this));
-    TrimBuffer();
+    TrimRawInputBuffer();
 }
 
 void Mouse::TrimBuffer() noexcept
@@ -160,21 +80,5 @@ void Mouse::TrimRawInputBuffer() noexcept
     while (rawDeltaBuffer.size() > bufferSize)
     {
         rawDeltaBuffer.pop();
-    }
-}
-
-void Mouse::OnWheelDelta(int x, int y, int delta) noexcept
-{
-    wheelDeltaCarry += delta;
-    // generate events for every 120 
-    while (wheelDeltaCarry >= WHEEL_DELTA)
-    {
-        wheelDeltaCarry -= WHEEL_DELTA;
-        OnWheelUp(x, y);
-    }
-    while (wheelDeltaCarry <= -WHEEL_DELTA)
-    {
-        wheelDeltaCarry += WHEEL_DELTA;
-        OnWheelDown(x, y);
     }
 }
