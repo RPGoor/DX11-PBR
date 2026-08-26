8b982db48a58750d54504d986d13f81da1efbc11
RPGoor <ryanpgoor@gmail.com>
2026-08-26 02:28:31 +0100

    small clean up

--- Source/Engine.Graphics/Core/Camera.h
+++ Source/Engine.Graphics/Core/Camera.h
@@ -4,16 +4,14 @@
 class Camera
 {
 public:
-	Camera() noexcept;
-	DirectX::XMMATRIX GetMatrix() const noexcept;
-	void SpawnControlWindow() noexcept;
-	void Reset() noexcept;
-	void Rotate( float dx,float dy ) noexcept;
-	void Translate( DirectX::XMFLOAT3 translation ) noexcept;
-	DirectX::XMFLOAT3 pos;
+    Camera() noexcept;
+    DirectX::XMMATRIX GetMatrix() const noexcept;
+    void Rotate( float dx,float dy ) noexcept;
+    void Translate( DirectX::XMFLOAT3 translation ) noexcept;
+    DirectX::XMFLOAT3 pos;
 private:
-	float pitch;
-	float yaw;
-	static constexpr float travelSpeed = 12.0f;
-	static constexpr float rotationSpeed = 0.004f;
+    float pitch;
+    float yaw;
+    static constexpr float travelSpeed = 12.0f;
+    static constexpr float rotationSpeed = 0.004f;
 };
